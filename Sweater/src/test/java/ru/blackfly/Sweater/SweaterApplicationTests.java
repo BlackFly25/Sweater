3bd01fbf84688976bbc87dcb88cd992dfbf91468
BlackFly25 <blackfly25@gmail.com>
2025-10-06 20:05:20 +0200

    Initial commit

---
+++ Sweater/src/test/java/ru/blackfly/Sweater/SweaterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.blackfly.Sweater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
