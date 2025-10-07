09af4a51388afaa18eccc6e0393dbaae194b0901
BlackFly25 <blackfly25@gmail.com>
2025-10-07 22:09:53 +0200

    Добавил собственную страницу с аутентификацией. Добавил страничку Error в случае возникновения ошибки. Полностью переписал класс SecurityConfig под новую версию SpringBoot. Добавил новый контроллер для аутентификации.

--- Sweater/src/main/java/ru/blackfly/Sweater/config/SecurityConfig.java
+++ Sweater/src/main/java/ru/blackfly/Sweater/config/SecurityConfig.java
@@ -2,12 +2,13 @@ package ru.blackfly.Sweater.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.blackfly.Sweater.services.PersonDetailsService;
 
 @EnableWebSecurity
@@ -19,9 +20,44 @@ public class SecurityConfig {
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
     }
+    //Настроим метод позволяющий настроить Spring security, а так же авторизацию
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//  Отключаем CSRF
+        http.csrf(AbstractHttpConfigurer::disable);
+//  Настраиваем авторизацию
+        http.authorizeHttpRequests((requests) ->
+                requests
+                        .requestMatchers("/auth/login", "/process_login", "/error").permitAll()
+                        .anyRequest().authenticated()
+        );
+// Настраиваем страницу логина
+        http.formLogin((form) ->
+                form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/auth/login?error")
+                        .permitAll()
+        );
+//  Настраиваем logout
+        http.logout((logout) ->
+                logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/auth/login?logout")
+                        .permitAll()
+        );
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(personDetailsService);
+        return http.build();
+    }
+
+    //Это метод для настройки аутентификации
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(personDetailsService);
+        provider.setPasswordEncoder(getPasswordEncoder());
+        return provider;
     }
 
     @Bean
